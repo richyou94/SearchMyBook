0877c17398768ca3463d8020bfbcfdb1085c1aff
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-19 16:55:14 +0900

    ha..

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://richardyou94:asdasdasd@atlascluster.xxu3hrq.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
