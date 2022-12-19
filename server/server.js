e3d2cb26b07f7da925de878877d426a44226e95d
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-19 10:00:09 +0900

    added schema folders and index.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
