cb2ee09158142cd2aadda08f6547e1f7582296d7
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-19 16:26:30 +0900

    adjust package.json and erased nodemodules and packagelock

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const db = require("./config/connection");
-// const routes = require("./routes");
-const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
-const app = express();
+// const routes = require("./routes");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +18,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
 
 const startApolloServer = async (typeDefs, resolvers) => {
   const apolloServer = new ApolloServer({
