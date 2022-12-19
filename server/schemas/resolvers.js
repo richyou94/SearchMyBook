5fe6e7dfc53d245b7a6899bca96edac7934cd982
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-19 10:26:23 +0900

    added adduser, and login mutation and user query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,42 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth')
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).select("-__v -password");
     },
-    Mutation: {
-
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      try {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        console.log("login error", err);
+      }
     },
+    login: async (parent, { email, password }) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new AuthenticationError("No user with this email address");
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect password");
+        }
+
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        console.log("login error", err);
+      }
+    },
+  },
 };
 
 module.exports = resolvers;
