dd6e565aac207709e7254ee04f98256d71bca264
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-19 11:35:13 +0900

    added typedefs in server file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,19 +2,43 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type User {
-        username: String
+        _id: ID!
+        username: String!
         email: String
-        password: String
+        bookCount: Int
         savedBooks: [Book]
     }
 
     type Book {
+        bookId: ID!
         authors: [String]
         description: String
-        bookId: String
         image: String
         link: String
-        title: String
+        title: String!
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
     }
 `;
 
