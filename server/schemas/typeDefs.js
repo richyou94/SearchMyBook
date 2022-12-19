b2c3338b7d76d522f57b8ece3945e845481b3ee8
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-19 10:14:40 +0900

    initialize the type=Defs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,21 @@
 const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type User {
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+`;
+
+module.exports = typeDefs;
